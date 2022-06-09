427199c8d447684784977d1ba370cfe4c3fad30f
Clément Hamada <clementhamada@protonmail.com>
2022-06-09 19:46:45 +0200

    layout: Rename fragments and move action button to the list fragment

--- app/src/main/java/me/chamada/ft_hangouts/EditFragment.kt
+++ app/src/main/java/me/chamada/ft_hangouts/EditFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import me.chamada.ft_hangouts.databinding.FragmentSecondBinding
+import me.chamada.ft_hangouts.databinding.FragmentContactEditBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class EditFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentContactEditBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,9 +22,9 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentContactEditBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +33,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_EditFragment_to_ListFragment)
         }
     }
 
--- app/src/main/java/me/chamada/ft_hangouts/ListFragment.kt
+++ app/src/main/java/me/chamada/ft_hangouts/ListFragment.kt
@@ -1,19 +1,20 @@
 package me.chamada.ft_hangouts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import me.chamada.ft_hangouts.databinding.FragmentFirstBinding
+import me.chamada.ft_hangouts.databinding.FragmentContactListBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentContactListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,20 +23,22 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentContactListBinding.inflate(inflater, container, false)
+
         return binding.root
-
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_ListFragment_to_EditFragment)
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
